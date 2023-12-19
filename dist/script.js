8aa410bdbc517bd2798f880509fd106138c0eecc
Realandreif <123496634+Realandreif@users.noreply.github.com>
2023-12-18 22:19:38 -0500

    Update script.js

--- dist/script.js
+++ dist/script.js
@@ -734,12 +734,7 @@ async function main() {
         viewMatrix = JSON.parse(decodeURIComponent(location.hash.slice(1)));
         carousel = false;
     } catch (err) {}
-    const url = new URL(
-        // "nike.splat",
-        // location.href,
-        params.get("url") || "truck.splat",
-        "https://huggingface.co/cakewalk/splat-data/resolve/main/",
-    );
+    const url = new URL("https://huggingface.co/cakewalk/splat-data/resolve/main/truck.splat");
     const req = await fetch(url, {
         mode: "cors", // no-cors, *cors, same-origin
         credentials: "omit", // include, *same-origin, omit
