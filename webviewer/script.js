98e508ec9d9367517e6dc460b480af2e21d07a71
Realandreif <123496634+Realandreif@users.noreply.github.com>
2023-12-19 12:56:34 -0500

    wetw

--- webviewer/script.js
+++ webviewer/script.js
@@ -734,7 +734,7 @@ async function main() {
         viewMatrix = JSON.parse(decodeURIComponent(location.hash.slice(1)));
         carousel = false;
     } catch (err) {}
-    const url = new URL("https://huggingface.co/cakewalk/splat-data/resolve/main/train.splat");
+    const url = new URL("/splats/train.splat");
     const req = await fetch(url, {
         mode: "cors", // no-cors, *cors, same-origin
         credentials: "omit", // include, *same-origin, omit
